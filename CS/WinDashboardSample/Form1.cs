169f12285c7cc86cd19759a8bac2104ac6749423
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2021-10-06 16:46:28 +0300

    Minor updates (include database; remove unnecessary files)

--- CS/WinDashboardSample/Form1.cs
+++ CS/WinDashboardSample/Form1.cs
@@ -1,25 +1,14 @@
-﻿using DevExpress.XtraEditors;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using ConstantLineExtension.Win;
-using System.IO;
+using DevExpress.XtraEditors;
 
-namespace WinDashboardSample
-{
-    public partial class Form1 : XtraForm
-    {
-        public Form1()
-        {
+namespace WinDashboardSample {
+    public partial class Form1 : XtraForm {
+        public Form1() {
             string dataDirectory = Directory.GetParent(Directory.GetParent(Directory.GetParent(Application.StartupPath).FullName).FullName).FullName;
             AppDomain.CurrentDomain.SetData("DataDirectory", dataDirectory);
-
 
             InitializeComponent();
             dashboardDesigner1.CreateRibbon();
